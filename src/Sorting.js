0c3ba3cd14d36bcb8dcebfdda2d48f3dcd932f10
Ihor Bulaievskyi <latente.traumgedanken@gmail.com>
2019-06-20 17:31:09 +0300

    sorting and filter

--- src/Sorting.js
+++ src/Sorting.js
@@ -1,22 +1,252 @@
 import React, { Component } from 'react';
-import Breadcrumbs from './Breadcrumbs';
+import ReactPaginate from 'react-paginate';
+import { geolocated } from 'react-geolocated';
+import { Button } from 'react-bootstrap';
+import {
+    ListGroup,
+    ListGroupItem,
+    ListGroupItemHeading,
+    ListGroupItemText
+} from 'reactstrap';
 
-export default class Sorting extends Component {
+import Breadcrumbs from './Breadcrumbs';
+import MyLoader from './Loader';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import './styles/Sorting.css';
+
+class Tile extends Component {
+    render() {
+        const centre = this.props.data;
+        return (
+            <ListGroupItem>
+                <ListGroupItemHeading>{centre.location}</ListGroupItemHeading>
+                <ListGroupItemText>
+                    Acceptable materials: <i>{centre.types.join(', ')}</i>
+                    <br />
+                    Distance: {parseInt(centre.distance)} meters
+                </ListGroupItemText>
+            </ListGroupItem>
+        );
+    }
+}
+
+class Sorting extends Component {
+    constructor(props) {
+        super(props);
+
+        this.perPage = 4;
+        this.state = {
+            sortByDistance: 0,
+            filtered: null,
+            centres: null,
+            activePage: 0,
+            filter: {
+                paper: false,
+                glass: false,
+                plastic: false
+            }
+        };
+
+        this._handlePageClick = this._handlePageClick.bind(this);
+        this._handleCheckBoxChange = this._handleCheckBoxChange.bind(this);
+        this._handleSortButtonClick = this._handleSortButtonClick.bind(this);
+    }
+
+    componentDidUpdate() {
+        if (!this.state.centres)
+            fetch(
+                `https://cors-anywhere.herokuapp.com/https://otp-trash.herokuapp.com/sorting`
+            )
+                .then(res => res.json())
+                .then(centres => {
+                    const processedArr = this._processResponse(centres);
+                    this.setState({
+                        centres: processedArr,
+                        fileted: processedArr
+                    });
+                });
+    }
+
     render() {
         return (
             <div>
-                <Breadcrumbs
-                    data={[
-                        { link: '/', name: 'HOME' },
-                        {
-                            link: '/activity/sorting',
-                            name: 'ACTIVITY',
-                            disabled: true
-                        },
-                        { link: '/activity/sorting', name: 'SORTING' }
-                    ]}
-                />
+                {this._renderBreadcrubms()}
+                {this.state.centres ? (
+                    <div className='main container'>
+                        <Button
+                            variant='outline-dark'
+                            size='sm'
+                            onClick={this._handleSortButtonClick}
+                        >
+                            Sort by distanse
+                        </Button>
+                        {this._renderCheckboxes()}
+                        {this._renderList()}
+                        {this._renderPagination()}
+                    </div>
+                ) : (
+                    <MyLoader />
+                )}
+            </div>
+        );
+    }
+
+    _processResponse(centres) {
+        return centres.map(centre => {
+            const foo = centre;
+            foo.distance = this._calculateDistance(foo);
+            return foo;
+        });
+    }
+
+    _calculateDistance(center) {
+        // approximate radius of earth in km
+        const R = 6373;
+
+        const lat1 = this.props.coords.latitude;
+        const lon1 = this.props.coords.longitude;
+        const lat2 = center.latitude;
+        const lon2 = center.longitude;
+
+        const dlon = lon2 - lon1;
+        const dlat = lat2 - lat1;
+
+        const a =
+            Math.sin(dlat / 2) ** 2 +
+            Math.cos(lat1) * Math.cos(lat2) * Math.sin(dlon / 2) ** 2;
+        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+        return R * c * 10;
+    }
+
+    _filter() {
+        let filtered = this.state.centres;
+        for (let filterBy in this.state.filter) {
+            if (this.state.filter[filterBy])
+                filtered = filtered.filter(
+                    centre => centre.types.indexOf(filterBy) !== -1
+                );
+        }
+        return filtered;
+    }
+
+    _handlePageClick(data) {
+        this.setState({ activePage: data.selected });
+    }
+
+    _handleCheckBoxChange(e) {
+        this.setState({
+            activePage: 0,
+            filter: {
+                ...this.state.filter,
+                [e.target.id]: e.target.checked
+            }
+        });
+    }
+
+    _handleSortButtonClick() {
+        const newSortValue =
+            this.state.sortByDistance === 0 ? 1 : -this.state.sortByDistance;
+        this.setState({ sortByDistance: newSortValue });
+    }
+
+    _renderPagination() {
+        if (!this.filtered) return;
+        return (
+            <ReactPaginate
+                previousLabel={'previous'}
+                nextLabel={'next'}
+                pageCount={this.filtered.length / this.perPage}
+                onPageChange={this._handlePageClick}
+                containerClassName={'pagination'}
+                subContainerClassName={'pages pagination'}
+                activeClassName={'active-page'}
+            />
+        );
+    }
+
+    _renderList() {
+        const start = this.state.activePage * this.perPage;
+        this.filtered = this._filter().sort(
+            (a, b) => this.state.sortByDistance * (a.distance - b.distance)
+        );
+        return (
+            <ListGroup>
+                {this.filtered
+                    .slice(start, start + this.perPage)
+                    .map((centre, i) => (
+                        <Tile key={centre.location + i} data={centre} />
+                    ))}
+            </ListGroup>
+        );
+    }
+
+    _renderBreadcrubms() {
+        return (
+            <Breadcrumbs
+                data={[
+                    { link: '/', name: 'HOME' },
+                    {
+                        link: '/activity/sorting',
+                        name: 'ACTIVITY',
+                        disabled: true
+                    },
+                    { link: '/activity/sorting', name: 'SORTING' }
+                ]}
+            />
+        );
+    }
+
+    _renderCheckboxes(name) {
+        return (
+            <div className='row checkboxes'>
+                <div className='main-container'>
+                    <p>PAPER</p>
+                    <div className='checkbox-container yellow'>
+                        <input
+                            type='checkbox'
+                            onChange={this._handleCheckBoxChange}
+                            id='paper'
+                        />
+                        <label htmlFor='paper' />
+                        <div className='active-circle' />
+                    </div>
+                </div>
+
+                <div className='main-container'>
+                    <p>PLASTIC</p>
+                    <div className='checkbox-container green'>
+                        <input
+                            type='checkbox'
+                            onChange={this._handleCheckBoxChange}
+                            id='plastic'
+                        />
+                        <label htmlFor='plastic' />
+                        <div className='active-circle' />
+                    </div>
+                </div>
+
+                <div className='main-container'>
+                    <p>GLASS</p>
+                    <div className='checkbox-container purple'>
+                        <input
+                            type='checkbox'
+                            onChange={this._handleCheckBoxChange}
+                            id='glass'
+                        />
+                        <label htmlFor='glass' />
+                        <div className='active-circle' />
+                    </div>
+                </div>
             </div>
         );
     }
 }
+
+export default geolocated({
+    positionOptions: {
+        enableHighAccuracy: false
+    },
+    userDecisionTimeout: 5000
+})(Sorting);
