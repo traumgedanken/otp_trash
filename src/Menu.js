d4b813b8302851b0530bee61801ffef925220944
Ihor Bulaievskyi <latente.traumgedanken@gmail.com>
2019-06-21 00:07:45 +0300

    social login

--- src/Menu.js
+++ src/Menu.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 import { LinkContainer } from 'react-router-bootstrap';
 import { Nav, Navbar, NavDropdown, Form } from 'react-bootstrap';
 import { GoogleLogin } from 'react-google-login';
-
-const responseGoogle = response => {
-    console.log(response);
-};
+import cookie from 'react-cookies';
 
 export default class Menu extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { user: cookie.load('user') };
+    }
+
     render() {
         return (
             <Navbar bg='light' expand='lg' fixed='top'>
@@ -32,17 +35,46 @@ export default class Menu extends Component {
                             <Nav.Link>CONTACTS</Nav.Link>
                         </LinkContainer>
                     </Nav>
-                    <Form inline>
-                        <GoogleLogin
-                            clientId='349786374195-8q7j1q1c251rvp0pmmcgkhmm5m736mlc.apps.googleusercontent.com'
-                            buttonText='Login'
-                            onSuccess={responseGoogle}
-                            onFailure={responseGoogle}
-                            cookiePolicy={'single_host_origin'}
-                        />
-                    </Form>
+                    <Form inline>{this._renderUserInfo()}</Form>
                 </Navbar.Collapse>
             </Navbar>
+        );
+    }
+
+    _handleGoogleResnonse(response) {
+        if (!response || response.error) return this.setState({ user: null });
+        const user = {
+            name: response.profileObj.name,
+            imageUrl: response.profileObj.imageUrl
+        };
+        cookie.save('user', user, { path: '/' });
+        this.setState({ user });
+    }
+
+    _renderUserInfo() {
+        if (this.state.user)
+            return (
+                <Navbar.Text>
+                    {this.state.user.name}
+                    <img
+                        style={{
+                            width: '40px',
+                            borderRadius: '40px',
+                            marginLeft: '5px'
+                        }}
+                        alt='user avatar'
+                        src={this.state.user.imageUrl}
+                    />
+                </Navbar.Text>
+            );
+        return (
+            <GoogleLogin
+                clientId='349786374195-8q7j1q1c251rvp0pmmcgkhmm5m736mlc.apps.googleusercontent.com'
+                buttonText='Login'
+                onSuccess={this._handleGoogleResnonse.bind(this)}
+                onFailure={this._handleGoogleResnonse.bind(this)}
+                cookiePolicy={'single_host_origin'}
+            />
         );
     }
 }
